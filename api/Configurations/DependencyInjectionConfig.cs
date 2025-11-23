7bd2055cdd279724cca65b8294c23f1903891b74
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-23 20:03:11 -0300

    feat(Configurations): create DependencyInjectionConfig

---
+++ api/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace api.Configurations;
+
+public static class DependencyInjectionConfig
+{
+    public static IServiceCollection AddDependencyInjectionConfiguration(this IServiceCollection services)
+    {
+
+
+        return services;
+    }
+}

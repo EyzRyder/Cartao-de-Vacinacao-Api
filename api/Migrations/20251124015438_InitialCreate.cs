4a498071d1d42b5719c190562a61fd11f804ce5d
Gabriel Bessi <gabriel.bessi.gb@gmail.com>
2025-11-23 23:10:27 -0300

    feat(DB): initialize databse

---
+++ api/Migrations/20251124015438_InitialCreate.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cartao_vacinacao_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vacinas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vacinas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VaccinationRecord",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    PersonId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    VaccineId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Dose = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    DateAplication = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VaccinationRecord", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VaccinationRecord_Pessoas_PersonId",
+                        column: x => x.PersonId,
+                        principalTable: "Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VaccinationRecord_Vacinas_VaccineId",
+                        column: x => x.VaccineId,
+                        principalTable: "Vacinas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VaccinationRecord_PersonId",
+                table: "VaccinationRecord",
+                column: "PersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VaccinationRecord_VaccineId",
+                table: "VaccinationRecord",
+                column: "VaccineId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VaccinationRecord");
+
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+
+            migrationBuilder.DropTable(
+                name: "Vacinas");
+        }
+    }
+}
